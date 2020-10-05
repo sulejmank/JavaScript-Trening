2070600ce5c7d8db808feba2a838e365caad1b65
Sulejman Karisik <sulejman.karisik@zuehlke.com>
2020-10-05 20:32:20 +0200

    Drugi komit

---
+++ test-projekat/src/app/heroes/heroes.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+}
